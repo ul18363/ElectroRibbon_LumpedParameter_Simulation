0b85858ee2ea45fa4d65738dfcd3b5965dd49969
Bruno <ul18363@bristol.ac.uk>
2020-06-29 04:43:46 +0100

    Bezier and Fast accurate COMSOL (also dt estimation working!)

--- Mechanical_Simulation/Mass Spring Models/09_MATLAB_GUI/Main_Manual_Param_estimator.m
+++ Mechanical_Simulation/Mass Spring Models/09_MATLAB_GUI/Main_Manual_Param_estimator.m
@@ -3,7 +3,7 @@ clear; clc;
 %profile on
 N=21;
 obj= Param_Estimator([0.1 0.0127 100e-6],N,'Steel AISI 4340',0.01);
-obj.M=0.02;% M=0 deformation in scale of 10^-5m -> 0.03mm
+obj.M=0.06;% M=0 deformation in scale of 10^-5m -> 0.03mm
 % M=0 deformation in scale of 10^-5m -> 0.03mm
 % M=0.06 deformation in scale of 10^-2m -> 17mm
 obj.update_model();
@@ -14,7 +14,8 @@ old_p=obj.plate.p;
 %%
 close all
 figure
-obj.set_damping_factor(9e2);%1e6 pretty close to critical (3e4 nice waves)
+%obj.set_damping_factor(9e2);%1e6 pretty close to critical (3e4 nice waves)
+obj.set_damping_factor(1e2);%1e6 pretty close to critical (3e4 nice waves)
 obj.plate.reset_state();
 obj.plate.p=old_p;
 %scale=1.55e-4;ratio=1;% Succesful for N=11;M=0
@@ -28,15 +29,15 @@ scale=5.45e-4;ratio=1;% Succesful for N=21;M=0
 
 %scale=1.55e-4;ratio=1;% Succesful for N=;M=
 
-scale_axial=scale*ratio;
-scale_trans=scale/ratio;
+scale_axial=1;%scale*ratio;
+scale_trans=5.7e-4;%scale/ratio;
 obj.plate.k_axial=og_k_axial*scale_axial;
 obj.plate.GA=og_GA*scale_trans;
 %obj.set_elastic_coefficients();
 %dt=4e-6; %Simulation timestep [s]
 %dt=1e-6; %Simulation timestep [s]
 %dt=1e-4;
-dt=1e-6;
+dt=1e-5;
 T=5; %Maximum simulation time [s]
 t_refresh=1e-2;
 run_simulation
