28482801ea74340fbc1f5c31fe412cb582de4859
Bruno Castro UoB <ul18363@bristol.ac.uk>
2020-08-12 01:37:20 +0100

    checkpoint

--- Mechanical_Simulation/Mass Spring Models/COMSOL_TO_MATLAB_SANDBOX/@ES_Estimator/ES_Estimator.m
+++ Mechanical_Simulation/Mass Spring Models/COMSOL_TO_MATLAB_SANDBOX/@ES_Estimator/ES_Estimator.m
@@ -1,50 +1,64 @@
 classdef ES_Estimator < handle
     properties
+        % Outsourced ES models
         comsol_model
         numerical_model
-%         bezier_points
+        
+        % Parameters of Model
+        % Geometrical parameters
         clip_l
         base_l
         sheet_width
-        %bE
-        gap
         voltage
         thickness
-%         model
-%         x
-%         y
-%         unTy
-%         unTx
-%         dFy
-%         dFx
-%         da
-%         cumFy
-%         cumFx
-%         numpoints
+        insulator_thickness
+        dropVolume
+        
+        % Physical parameters
+        eMed
+        eAir
+        eIns
+        EMaxMed 
+        EMaxAir
     end
     methods
-        function obj=ES_Estimator(initial_points,thickness,insulator_thickness,sheet_width,clip_l,base_l,voltage)
-            % Constructor
-%             obj.bezier_points=bezier_points;
+        function obj=ES_Estimator(thickness,insulator_thickness,sheet_width,clip_l,base_l,voltage)
+            % Constructor with general guidelines
             obj.thickness=thickness;
             obj.sheet_width=sheet_width;
             obj.clip_l=clip_l;
             obj.base_l=base_l;
             obj.voltage=voltage;
-            curve_points= [initial_points(1,:)+[-clip_l 0];...
+            obj.insulator_thickness=insulator_thickness;
+            
+            % Initialize parallel plate model parameters
+            obj.eMed = 2.75; obj.eAir = 1; obj.eIns = 4.62; 
+            obj.EMaxMed = 20e6; obj.EMaxAir = 3e6; obj.dropVolume = 10;
+
+        end
+        
+        function add_comsol_model(obj,initial_points)
+            % Adds COMSOL model
+            % It will probably break if all points are aligned
+            curve_points= [initial_points(1,:)+[-obj.clip_l 0];...
                 initial_points;...
-                initial_points(end,:)+[base_l/2 0]];
+                initial_points(end,:)+[obj.base_l/2 0]];
+            
             obj.comsol_model=COMSOL_ES_Model(curve_points,...
-                thickness,insulator_thickness,sheet_width,voltage);
+                obj.thickness,obj.insulator_thickness,obj.sheet_width,obj.voltage);
+            
             obj.comsol_model.calculate_es_force(initial_points)
-            % Initialize parallel plate model
-            eMed = 2.75; eAir = 1; eIns = 4.62; 
-            tIns = 130e-6; EMaxMed = 20e6; EMaxAir = 3e6; dropVolume = 10;
+        end
+        
+        function add_analytical_model(obj)
             obj.numerical_model=Parallel_Plates_ES_Model(initial_points,...
-                voltage,eMed,eAir,eIns,tIns,EMaxMed,EMaxAir,dropVolume,...
-                sheet_width);
+                obj.voltage,obj.eMed,obj.eAir,obj.eIns,...
+                obj.insulator_thickness,...
+                obj.EMaxMed,obj.EMaxAir,obj.dropVolume,...
+                obj.sheet_width);
             obj.numerical_model.calculate_es_force()
         end
+        
         
         [Fy,Fx,arc_len_sim]=assign_distribute_forces_to_particles(obj,points,source)
     end
